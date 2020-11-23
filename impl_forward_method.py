4948c50da62745203091387fe78c522849effa21
Divakar Verma <divakarv96@gmail.com>
2020-11-23 09:35:19 +0530

    Pass img, batch to forward method

--- impl_forward_method.py
+++ impl_forward_method.py
@@ -1,6 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+torch.set_grad_enabled(False) # Disabling Training stuff / Computational Graph for now to save mem
+
+train_set = torchvision.datasets.FashionMNIST(
+    root = './data/',
+    train=False, #should be True though
+    download=False,
+    transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+
+train_loader = torch.utils.data.DataLoader(
+    train_set, batch_size = 10
+)
 
 class MyNet(nn.Module):
     def __init__(self):
@@ -38,3 +56,27 @@ class MyNet(nn.Module):
 net = MyNet()
 net.forward(torch.rand(10,1,28,28))
 print(net)
+
+img, label = next(iter(train_set))
+output = net.forward(img.unsqueeze(axis=0))
+print(output.shape)
+print(output)
+print(output.argmax(dim=1))
+# print(output.argmax(dim=0))
+print("Actual label: ", label)
+# plt.imshow(img.squeeze())
+# plt.show()
+
+print("~"*10, "Batch", "~"*10)
+img_set, label_set = next(iter(train_loader))
+print("Shape of img set: ", img_set.shape)
+output_set = net.forward(img_set)
+print("Shape of out set: ", output_set.shape)
+print("Predicted output: ", output_set.argmax(dim=1))
+print("Actual Labels: \t", label_set)
+print("Checking match: ", output_set.argmax(dim=1).eq(label_set))
+print("Checking match [No.]: ", output_set.argmax(dim=1).eq(label_set).sum().item())
+'''Can use softmax followed by argmax'''
+
+def get_num_correct(predicted, actual):
+    return predicted.argmax(dim=1).eq(actual).sum().item()
